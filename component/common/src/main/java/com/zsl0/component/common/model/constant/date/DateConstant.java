5b122b6586d1af045a20666cd8201f9b9eb8024f
zsl0 <249269610@qq.com>
2023-07-12 14:12:14 +0800

    feat(common): 添加时间格式校验注解

---
+++ component/common/src/main/java/com/zsl0/component/common/model/constant/date/DateConstant.java
@@ -0,0 +1,26 @@
+package com.zsl0.component.common.model.constant.date;
+
+/**
+ * @author zsl0
+ * created on 2023/7/12 10:27
+ */
+public interface DateConstant {
+
+    /**
+     * 数据库timestamp对应的时间格式
+     */
+    String DB_TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 共用日期通配符
+     */
+    String ymdhms = "yyyyMMddHHmmss";
+    String ymdhm = "yyyyMMddHHmm";
+    String ymdh = "yyyyMMddHH";
+    String ymd = "yyyyMMdd";
+    String ym = "yyyyMM";
+    String y = "yyyy";
+    String m = "MM";
+    String hm = "HHmm";
+
+}
